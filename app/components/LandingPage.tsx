32a977aa787cb8217da190c24de3cadab19cad7c
Greg Alboum <alboumgr@msu.edu>
2024-10-04 00:54:04 -0400

    Updates to run on Vercel

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,14 +1,14 @@
 // components/LandingPage.tsx
 import Image from 'next/image';
-import potluckImage from '../public/potluck.jpg';
-import logo from '../public/logo.png';
+// import potluckImage from '../public/potluck.jpg';
+// import logo from '../public/logo.png';
 
 const LandingPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100">
-      <Image src={logo} alt="PotLockToken Logo" width={200} height={200} />
+      <Image src={"/logo.png"} alt="PotLockToken Logo" width={200} height={200} />
       <h1 className="text-4xl font-bold mt-4">What will you bring to the PotLock?</h1>
-      <Image src={potluckImage} alt="Potluck Dinner" width={600} height={400} className="mt-4 rounded-lg" />
+      <Image src={"/potluckImage.png"} alt="Potluck Dinner" width={600} height={400} className="mt-4 rounded-lg" />
       <div className="mt-8 px-4">
         <p className="text-lg">Description of the token and its contract address.</p>
         <p className="text-lg">Tokenomics details here.</p>
